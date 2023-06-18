a8008f807126857fc9d9e2786988db292a987e3e
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-06-18 17:13:42 +0530

    feat: Home cleanups

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,56 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  setRandomCocktails,
-  setSearchResults,
-  addToFavorites,
-  removeFromFavorites,
-} from "./store";
-import { fetchRandomCocktails, searchCocktails } from "./services/cocktailApi";
-import { RootState } from "./store";
-import Cocktail from "./modules/Cocktail/Cocktail";
 import RandomItems from "./views/RandomItems";
 import SearchResults from "./views/SearchResults";
 import Favorites from "./views/Favorites";
-import { CocktailType } from "./modules/Cocktail/types/CocktailType";
+import { FC } from "react";
 
-const Home: React.FC = () => {
-  const dispatch = useDispatch();
-  const { randomCocktails, searchResults, favorites } = useSelector(
-    (state: RootState) => state.cocktails
-  );
-  const [searchTerm, setSearchTerm] = useState("");
-
-  useEffect(() => {
-    const fetchCocktails = async () => {
-      try {
-        const cocktails = await fetchRandomCocktails(5);
-        dispatch(setRandomCocktails(cocktails));
-      } catch (error) {
-        // Handle error
-      }
-    };
-
-    fetchCocktails();
-  }, []);
-
-  const handleSearch = async () => {
-    try {
-      const results = await searchCocktails(searchTerm);
-      dispatch(setSearchResults(results));
-    } catch (error) {
-      // Handle error
-    }
-  };
-
-  const handleAddToFavorites = (cocktail: CocktailType) => {
-    dispatch(addToFavorites(cocktail));
-  };
-
-  const handleRemoveFromFavorites = (cocktailId: string) => {
-    dispatch(removeFromFavorites(cocktailId));
-  };
-
+const Home: FC = () => {
   return (
     <div>
       <h1>Home</h1>
